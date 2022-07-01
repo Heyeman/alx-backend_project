5ceffb872fd4bb177979b879b6366cff92a8db23
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-07-01 21:45:15 +0300

    Done with the request status options

--- backend/middlewares/keyChecker.js
+++ backend/middlewares/keyChecker.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler"),
-  { apiModel } = require("../models/apiKeyModel");
+  { apiModel } = require("../models/apiKeyModel"),
+  { ObjectId } = require("mongoose").Schema.Types;
+
 module.exports = asyncHandler(async (req, res, next) => {
   let apiKey = req.headers["request-key"];
   if (!apiKey) {
@@ -11,6 +13,23 @@ module.exports = asyncHandler(async (req, res, next) => {
     res.status(400);
     throw new Error("API request key not found");
   } else {
+    const requestOptions = {
+      status: true,
+      apiKey: key.apiKey,
+      method: req.method === "GET" ? "Get" : "Check",
+      resources: {
+        subject: req.subject,
+        grade: req.grade,
+        year: req.year,
+        chapter: req.params.chapter,
+        requestType: req.params.chapter
+          ? "By chapter"
+          : req.grade
+          ? "By grade"
+          : "All",
+      },
+    };
+
     res.on("error", () => {
       console.log("finished with error");
     });
