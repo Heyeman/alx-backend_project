725f44f77ab7d019c84d2ba745aca24c0342091c
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-07-01 20:13:26 +0300

    Setup api key checker

--- backend/middlewares/keyChecker.js
+++ backend/middlewares/keyChecker.js
@@ -1,4 +1,22 @@
-const asyncHandler = require("express-async-handler");
+const asyncHandler = require("express-async-handler"),
+  { apiModel } = require("../models/apiKeyModel");
 module.exports = asyncHandler(async (req, res, next) => {
-  let apiKey = req.headers["APi"];
+  let apiKey = req.headers["request-key"];
+  if (!apiKey) {
+    res.status(400);
+    throw new Error("No API key provided");
+  }
+  const key = await apiModel.findOne({ apiKey });
+  if (!key) {
+    res.status(400);
+    throw new Error("API request key not found");
+  } else {
+    if (req.method == "POST" && key.type === "basic") {
+      res.status(403);
+      throw new Error(
+        "Unauthorised to access this resource with this API request key"
+      );
+    }
+    req.key = key;
+  }
 });
