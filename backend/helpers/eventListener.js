6247d5ceb2de1da98ae537695b5748b4d9fa6c3b
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-11-02 22:05:45 +0300

    Removed console log lines

--- backend/helpers/eventListener.js
+++ backend/helpers/eventListener.js
@@ -2,7 +2,6 @@ const { statusModel } = require("../models/resStatusModel");
 module.exports = require("express-async-handler")(async () => {
   try {
     const savedStatus = await statusModel.create(options);
-    console.log("status saved");
   } catch (error) {
     throw new Error(error.message);
   }
